535b7d27f14bdd3e1e13832bc558d77633e44c1f
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-23 19:49:49 -0300

    Jpa repository, dependency injection, database seeding

---
+++ src/main/java/com/example/aulajpa/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.example.aulajpa.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.aulajpa.entities.User;
+import com.example.aulajpa.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Willian Brendo", "willian@gmail.com", "99999", "12345");
+		User u2 = new User(null, "Brendo", "brendo@gmail.com", "88888", "12345");
+		
+		repository.saveAll(Arrays.asList(u1, u2));
+	}
+	
+	
+}
