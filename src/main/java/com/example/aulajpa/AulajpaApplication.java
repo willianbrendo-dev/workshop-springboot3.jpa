77eef11a270c9ce204b03c3c5cb895354a06c4ad
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-23 17:17:39 -0300

    Project created

---
+++ src/main/java/com/example/aulajpa/AulajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aulajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulajpaApplication.class, args);
+	}
+
+}
